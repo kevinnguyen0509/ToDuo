6bb28d3492d577222ce24849e804be3c71f33d06
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2022-04-13 22:43:31 -0700

    Login set up and dashboard complete
    Still need Boards to be shown and ability to add new Adventure

---
+++ ToDuo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDuo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string LoginViewPath = "~/Views/Login/Login.cshtml";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View(LoginViewPath);
+        }
+
+    }
+}
---
+++ ToDuo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToDuo.MvcApplication" Language="C#" %>
