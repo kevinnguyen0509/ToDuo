9f06f0a74efc820d2124b369d543597c17308553
kevinnguyen0509 <Kevin.Nguyen0509@gmail.com>
2022-06-27 20:26:50 -0700

    Separated Index into partial views

--- ToDuo/Controllers/HomeController.cs
+++ ToDuo/Controllers/HomeController.cs
@@ -9,9 +9,10 @@ namespace ToDuo.Controllers
     public class HomeController : Controller
     {
         public static string LoginViewPath = "~/Views/Login/Login.cshtml";
+        public static string IndexPath = "~/Views/Home/Dashboard/Index.cshtml";
         public ActionResult Index()
         {
-            return View();
+            return View(IndexPath);
         }
 
         public ActionResult Login()
